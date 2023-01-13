4b838d55ed48d3ea30834e3a2ccc19df4920d9cb
radmanlo <radman.lotfiazar@gmail.com>
2023-01-13 17:45:23 +0300

    WebClient is changed to WebClient.Builder in order to transfer data from different instance of inventory.

--- Online Shopping Microservices/Order Service/src/main/java/com/order/service/OrderService.java
+++ Online Shopping Microservices/Order Service/src/main/java/com/order/service/OrderService.java
@@ -21,7 +21,7 @@ import java.util.UUID;
 public class OrderService {
 
     private final OrderRepository orderRepository;
-    private final WebClient webClient;
+    private final WebClient.Builder webClientBuilder;
     public void placeOrder(OrderRequest orderRequest){
         Order order = new Order();
         order.setOrderNumber(UUID.randomUUID().toString());
@@ -40,7 +40,7 @@ public class OrderService {
          * Call Inventory Service in order to
          * check the availability of product
          */
-        InventoryResponse[] inventoryResponses = webClient.get().uri("http://localhost:8082/api/inventory",
+        InventoryResponse[] inventoryResponses = webClientBuilder.build().get().uri("http://inventory-service/api/inventory",
                         uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
                 .retrieve()
                 .bodyToMono(InventoryResponse[].class)
