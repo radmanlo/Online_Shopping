23aec73c0f4ee606af6df984e5a385040814bedc
radmanlo <radman.lotfiazar@gmail.com>
2023-01-13 17:21:00 +0300

    Eureka server and Clients are connected successfully.

--- Online Shopping Microservices/Inventory Service/src/main/java/com/inventory/InventoryServiceApplication.java
+++ Online Shopping Microservices/Inventory Service/src/main/java/com/inventory/InventoryServiceApplication.java
@@ -1,9 +1,13 @@
 package com.inventory;
 
+import jdk.jfr.Enabled;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class InventoryServiceApplication {
 
     public static void main(String[] args) {
